5a39790557d5fa603954ba287681695107eed8a7
dan <da.ga.mo.94@gmail.com>
2022-03-16 00:51:53 -0600

    add code to increment by two instead of one by using a calback function - see lines16-17

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -3,20 +3,25 @@ import React, { useState } from "react";
 function Subscribe() {
   const [subscribed, setSubscribed] = useState(false); // useState(false) sets the initial value of 'subscribe' to false
   const [alerts, setAlerts] = useState(false);
-  const [counter, setCounter] = useState(0);
+  const [subscribedCount, setSubscribedCount] = useState(0);
   console.log("Subscribed status:", subscribed);
   console.log("Alerts status:", alerts);
   return (
     <section>
-      <p>Please click to subscribe to updates.</p>
-      <button onClick={() => setSubscribed(!subscribed)}>
+      <p>Please click to subscribe to my updates!</p>
+      <p>Subscriber Count: {subscribedCount}</p>
+      <button
+        onClick={() => {
+          setSubscribed(!subscribed);
+          setSubscribedCount( currentCount => currentCount + 1);
+          setSubscribedCount( currentCount => currentCount + 1);
+          if (!alerts) setAlerts(true);
+        }}
+      >
         {subscribed ? "Unsubscribe" : "Subscribe"}
       </button>
       <button onClick={() => setAlerts(!alerts)}>
         {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
-      </button>
-      <button onClick={() => setCounter(counter + 1)}>
-        Click counter: {counter}
       </button>
     </section>
   );
